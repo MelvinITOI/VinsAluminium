3effeca2539698fa0625645e87f817d8a6e47d03
MelvinITOI <melvin@itoi.org>
2023-11-16 20:59:08 +0530

    side bar component added

--- src/component/dashboard/dashboard.component.ts
+++ src/component/dashboard/dashboard.component.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule,SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
